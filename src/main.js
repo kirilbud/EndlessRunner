521f3310abaa7e4f67dcb8f31c2ff616e291c3ba
kirilbud <kiril.saltz@gmail.com>
2025-01-27 18:48:17 -0800

    init

---
+++ src/main.js
@@ -0,0 +1,30 @@
+/*
+Name: Kiril Saltz
+Project title: Cubicle Collateral
+Hours: 0
+creative tilt:
+Sources:
+Music: made in Beepbox
+sfx: made in sfxr.me
+Sprites: Made in Aseprite
+in game music and art is made by me
+*/
+
+
+//game/phaser config
+let config = {
+    type: Phaser.AUTO,
+    width: 640,
+    height: 480,
+    scene: [Menu, Play]
+}
+
+
+let game = new Phaser.Game(config)
+
+//ui stuff
+let borderUISize = game.config.height/15
+let borderPadding = borderUISize / 3
+
+//keyboard
+let keyFIRE, keyRESET, keyLEFT, keyRIGHT
